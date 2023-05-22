d8d986a5507b3d48751027528049a99b8d213b23
Luiz Gustavo Lucio Pereira <74224127+gusta4065@users.noreply.github.com>
2023-05-22 02:39:33 +0000

    SIM

--- IC2/Av1/fuzzy/fuzzy.py
+++ IC2/Av1/fuzzy/fuzzy.py
@@ -10,18 +10,26 @@ class Fuzzy:
     def addRules(self,name,start, end):
         newRule = {'name':name, 'limits':{'start': start,'end' :end}}
         self.rules.append(newRule)
+    def getRulesNames(self):
+        return [item['name'] for item in fuzzyTeste.rules]
+
     def premiseLowerB(self,a,b,x):
         return 1 - (b-x)/(b-a)
     def premiseHigherB(self,c,b,x):
         return 1 - (c-x)/(c-b)
 
 
+#def search():
+
 
 fuzzyTeste = Fuzzy(250)
+
+
+
 fuzzyTeste.addRules('Calor_Fraco',50,80)
 fuzzyTeste.addRules('Calor_Medio',65,130)
 fuzzyTeste.addRules('Calor_Forte',110,180)
 fuzzyTeste.addRules('Calor_Critico',165,fuzzyTeste.xMax)
-print(fuzzyTeste.rules)
+
 
 
